bf66eae44688f23d69c47ece00652e5a63d16598
bilguun1130 <beek1130@gmail.com>
2023-10-23 13:51:29 +0800

    package json changed

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -49,7 +49,7 @@ const TestiMonials = () => {
                 items: 1
             },
             600: {
-                items: 3
+                items: 2
             },
             1000: {
                 items: 3
